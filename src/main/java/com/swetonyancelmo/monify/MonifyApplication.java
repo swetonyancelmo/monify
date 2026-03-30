754a802a6f33317d7afecd9ff5d920655e7d0a28
Swetony <swetonyancelmo@gmail.com>
2026-03-29 22:37:02 -0300

    chore: commit inicial

---
+++ src/main/java/com/swetonyancelmo/monify/MonifyApplication.java
@@ -0,0 +1,13 @@
+package com.swetonyancelmo.monify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swetonyancelmo/monify/MonifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swetonyancelmo.monify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
